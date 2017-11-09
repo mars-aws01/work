22b00cff024ac778d60f666e3ec30ed228565d5f
Jay.M.Hu <jay.m.hu@newegg.com>
2017-11-09 09:36:31 +0800

    Remove overlay

--- src/core/index.ts
+++ src/core/index.ts
@@ -10,27 +10,12 @@ import {
   positionElements
 } from './positioning';
 
-import { ConnectedOverlayDirective } from './overlay/connected-overlay.directive';
-import { Overlay } from './overlay/overlay.service';
-import { OverlayContainer } from './overlay/overlay-container.service';
-import { OverlayOriginDirective } from './overlay/overlay-origin.directive';
-import { OverlayRef } from './overlay/overlay-ref';
-
-export const CORE_DIRECTIVES: any[] = [
-  ConnectedOverlayDirective,
-  OverlayOriginDirective
-];
-
 export const CORE_SERVICES: any[] = [
-  Overlay, OverlayContainer,
   ComponentLoaderFactory,
   PositioningService
 ];
 
 export {
-  Overlay,
-  OverlayRef,
-  OverlayContainer,
   ComponentLoader,
   ComponentLoaderFactory,
   ContentRef,
--- src/kubi-ui.module.ts
+++ src/kubi-ui.module.ts
@@ -1,9 +1,9 @@
 import './styl/all.styl';
 
 import { ALL_COMPONENTS, ENTRY_COMPONENTS } from './components';
-import { CORE_DIRECTIVES, CORE_SERVICES } from './core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { CORE_SERVICES } from './core';
 import { CommonModule } from '@angular/common';
 import { HELPERS } from './helpers';
 import { NgModule } from '@angular/core';
@@ -11,9 +11,9 @@ import { NkValidators } from './components/validators';
 
 @NgModule({
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  declarations: [...ALL_COMPONENTS, ...CORE_DIRECTIVES],
+  declarations: [...ALL_COMPONENTS],
   entryComponents: [...ENTRY_COMPONENTS],
-  exports: [...ALL_COMPONENTS, ...CORE_DIRECTIVES],
+  exports: [...ALL_COMPONENTS],
   providers: [...HELPERS, ...CORE_SERVICES, NkValidators]
 })
 export class KubiUIModule {}
