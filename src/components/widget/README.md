4938ac4069b867152ad6e047173f62691d78ceaf
Jay.M.Hu <jay.m.hu@newegg.com>
2017-11-28 14:28:54 +0800

    Update widget component

--- src/components/widget/README.md
+++ src/components/widget/README.md
@@ -1,3 +1,25 @@
 # nk-widget 
 
-该组件和 `nk-collapse-box` 合并为一个，请查询 `nk-collapse-box` 的文档。
+**Input**
+
+| 属性         | 说明                         | 类型    | 默认值 |
+| ------------ | ---------------------------- | ------- | ------ |  |
+| header       | 设置 `collapse-box` 的Header | string  | -      |
+| collapsed    | 设置box是否是收缩状态        | boolean | false  |
+| defaultColor | 设置标题栏背景色             | string  |        |
+ 
+**Slots（ng-content占位符）**
+
+| Name    | Description                                  |
+| ------- | -------------------------------------------- |
+| ''      | 放置内容                                     |
+| header  | 用于自定义Header                             |
+| toolbar | 用于在标题栏右侧放置工具元素（链接或者按钮） |
+
+**Events（Output）**
+
+| 属性            | 说明               | 参数值类型          |
+| --------------- | ------------------ | ------------------- |  |
+| collapsedChange | 收缩状态切换时触发 | boolean（收缩状态） |
+
+
--- src/components/widget/widget.component.ts
+++ src/components/widget/widget.component.ts
@@ -1,6 +1,6 @@
 import './widget.component.styl';
 
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'nk-widget',
@@ -12,11 +12,27 @@ export class WidgetComponent implements OnInit, OnChanges {
   public innerCollapsed: boolean = false;
 
   @Input() header: string;
+  @Input() icon: string;
   @Input() collapsed: boolean = false;
   @Input() disabled: boolean = false;
+  @Input() defaultColor: string = '';
+  @Input() showCollapseBtn: boolean = true;
+  @Input() showFullscreenBtn: boolean = false;
+  @Input() showPickColorBtn: boolean = false;
+  @Output() collapsedChange: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  @Output()
-  public collapsedChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @ViewChild('customHeader') customHeader: ElementRef;
+  public get hasCustomHeader() {
+    return this.customHeader.nativeElement.children.length > 0;
+  }
+
+  public get headerStyle() {
+    const style = {};
+    if (this.defaultColor) {
+      style['background-color'] = this.defaultColor;
+    }
+    return style;
+  }
 
   ngOnInit() { }
 
